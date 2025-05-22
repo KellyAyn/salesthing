3f97d22cc94190e04cd4dd17a3974e0301460cc6
RayAug <vojtech.aug@gmail.com>
2025-05-22 22:29:50 +0200

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 'use client';
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
-import { handleExcelUpload } from "~/server/utils/handleExcelUpload";
+//import { handleExcelUpload } from "~/server/utils/handleExcelUpload";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <Button className="bg-red-500"
-          onClick={handleExcelUpload}>
+        <Button className="bg-red-500">
           Upload Excel NOT IMPLEMENTED!!
         </Button>
       </div>
