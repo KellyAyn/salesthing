b329c7c2f55f095fbd6f48e4a7954abb77f07aeb
otatormeps <oliverhawk2505@gmail.com>
2025-05-24 22:15:37 +0100

    testing page

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <p>testing</p>
+import { db } from "~/server/db";
+import { leads } from "~/server/db/schema";
+import { Suspense } from "react";
+import { DataTable } from "~/components/data-table";
+import { columns } from "~/components/ui/columns";
+
+export default async function Page() {
+
+    const data = await db.select().from(leads).limit(50)
+
+  return (
+    <div className="container mx-auto w-full">
+      <Suspense
+        fallback={<div className="text-center text-2xl font-bold">Loading...</div>}
+        >
+        <DataTable columns={columns} data={data} />
+      </Suspense>
+    </div>
+  )
+  
 }
