8dd1023c93b42ffcdaaa094e5399a40c36d80ee2
RayAug <vojtech.aug@gmail.com>
2025-05-25 05:13:18 +0200

    excel parsing

--- src/app/actions/excel-processor.ts
+++ src/app/actions/excel-processor.ts
@@ -1,13 +1,39 @@
 "use server";
 
+import { inArray } from "drizzle-orm";
 import * as XLSX from "xlsx";
+import { db } from "~/server/db";
+import { leads } from "~/server/db/schema";
+import { revalidatePath } from "next/cache";
+import { auth } from "@clerk/nextjs/server";
 
-export async function processExcel(file: File) {
-    const workbook = XLSX.read(file, { type: 'buffer' });
+export async function processExcel(file: ArrayBuffer) {
+    const { userId } = await auth();
+    const workbook = XLSX.read(file, { type: 'array' });
     const sheetName = workbook.SheetNames[0];
-    if (!sheetName) return "No sheet found" //implement proper toast message;
+    if (!sheetName) {
+        throw new Error("No sheets found in workbook");
+    }
     const sheet = workbook.Sheets[sheetName];
-    if (!sheet) return "No sheet found" //implement proper toast message;
-    const jsonData = XLSX.utils.sheet_to_json(sheet);
-    return jsonData;
+    const jsonData = XLSX.utils.sheet_to_json<string[]>(sheet!, { header: 1 });
+
+    const domains = jsonData.map((row) => row[0]).filter((domain): domain is string => domain !== undefined);
+    
+    const match = await db.select().from(leads).where(inArray(leads.domain, domains));
+    const filteredDomains = domains.filter((domain) => !match.some((lead) => lead.domain === domain));
+    console.log(filteredDomains);
+    const newProspects = filteredDomains.map((domain) => (
+        {
+            domain: domain,
+            status: "prospect",
+            lastUpdate: new Date(),
+            ownerID: userId
+        }
+    ))
+
+    console.log(newProspects);
+
+    revalidatePath("/prospecting");
+
+    return filteredDomains;
 }
--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -3,17 +3,41 @@
 import { Upload } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { processExcel } from "~/app/actions/excel-processor";
+import { processExcel } from "~/app/actions/excel-processor";   
+import { toast } from "sonner";
+
 
 export function ExcelUploader() {
-
     const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files;
         if (!files || files.length === 0) return;
         const selectedFile = files[0];
         if (!selectedFile) return;
-        const result = await processExcel(selectedFile);
-        console.log(result);
+        const fileExtension = selectedFile.name.split('.').pop()?.toLowerCase();
+        console.log(fileExtension);
+        if (fileExtension !== 'xlsx' && fileExtension !== 'xls') {
+            toast.error('Please upload an Excel file (.xlsx or .xls)', {
+                icon: '🤦‍♂️',
+                description: 'You fucking idiot, kys',
+                duration: 5000,
+                richColors: true,
+            });
+            return;
+        }
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            const arrayBuffer = e.target?.result;
+            if (!arrayBuffer) return;
+            const result = await processExcel(arrayBuffer as ArrayBuffer);
+            console.log(result);
+        }
+        reader.readAsArrayBuffer(selectedFile);
+
+        toast.success('Successfully processed the excel file.', {
+            icon: '👍',
+            duration: 5000,
+            richColors: true,
+        });
  }   
 
     return (
