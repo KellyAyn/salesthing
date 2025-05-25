07c1a033f2f6cdc7997269c2a3d023fbec499587
otatormeps <oliverhawk2505@gmail.com>
2025-05-25 02:07:51 +0100

    sus

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,10 +3,12 @@ import { redirect } from 'next/navigation'
 
 export default async function AdminDashboard() {
   // Protect the page from users who are not admins
+  
   const isAdmin = await checkRole('admin')
   if (!isAdmin) {
     redirect('/')
   }
-
-  return <p>This is the protected admin dashboard restricted to users with the `admin` role.</p>
+  
+return <p className="text-center text-2xl font-bold justify-center items-center">you are a sussy baka</p>;
 }
+
